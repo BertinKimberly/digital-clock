f1f90173fffc510c866c8277808220e6b4965d63
BertinKimberly <122426811+BertinKimberly@users.noreply.github.com>
2023-10-10 10:06:26 +0200

    made it responsive

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function updateClock() {
     let m = new Date().getMinutes()
     let s = new Date().getSeconds()
     let ampm = "AM"
-
+    
     if (h > 12) {
         h = h - 12
         ampm = "PM"
